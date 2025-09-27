3a6604b10580dbd56b4708a604b0d44729be3c14
H0llyW00dzZ <h0llyw00dzz@pm.me>
2025-09-28 04:45:20 +0700

    Refactor Documentation
    - [+] refactor(mcp/magic_embedded_fs.go): rename magic_embedding.go to magic_embedded_fs.go and update docstrings

--- mcp/magic_embedded_fs.go
+++ mcp/magic_embedded_fs.go
@@ -2,7 +2,7 @@ package mcp
 
 import "embed"
 
-// Note: Do not remove this magic embedding.
+// Note: Do not remove this magic embedded template filesystem.
 
 //go:embed docs/templates/*.tmpl
 //go:embed docs/templates/instructions/*.tmpl
